f1fe542da73a40616e8456c62cce27e8e00d265c
PradipBishwokarma <pro.pradip100@gmail.com>
2025-12-04 13:18:59 +0545

    first commit

---
+++ Introduction to Python/Practicals/exercise1.py
@@ -0,0 +1,19 @@
+'''
+Running your first Python script
+You're starting to build your first ever Python project as a developer. Before adding features, you need to verify that everything is working correctly. One way to test this is by using the print() function to display a message. One has been provided for you here. You can run the code as many times as you want, and when you're ready let's mpve to next step.
+'''
+
+# Your first print function
+print("Hello, world!")
+
+
+# Write a print statement that displays the message Welcome to the recipe scaler.
+print("Welcome to the recipe scaler")
+
+
+'''
+Writing your first Python script
+Now that you've verified things are working, it's time to personalize it! As you code more, you'll need to display different messages. Remember, you can write any message you want inside the quotation marks. This time, instead of running a pre-written message, you'll craft your own message.
+'''
+
+
---
+++ Introduction to Python/Practicals/exercise2.py
@@ -0,0 +1,49 @@
+'''
+Storing variables
+You're building your first Python project - a recipe scaler! Before you can scale recipes, you need to learn how to store ingredient information. In Python, you use variables to store values you'll use multiple times. For recipes, you'll need to store ingredient names (text, called strings) and quantities (whole numbers, called integers). Let's start by storing information for a pasta recipe.
+'''
+
+'''
+Create a variable called pasta_type and store the string "spaghetti" in it.
+Create a variable called quantity and store the integer 80 in it, for grams.
+Print both variables to verify they were stored correctly.
+
+'''
+
+# Store the pasta type as a string
+pasta_type = "spaghetti"
+
+# Store the quantity as an integer
+quantity = 80
+
+# Print the pasta type
+print(pasta_type)
+
+# Print the quantity
+print(quantity)
+
+
+'''
+Updating variables
+You've stored the initial values for a pasta recipe, but now you realize you need to make some adjustments. You prefer "fusilli" pasta instead of spaghetti, and you've decided to use more of it. In Python, you can easily update variables by reassigning them new values, just like you did when you first created them.
+
+Let's practice updating your recipe variables and printing the new values to confirm the changes.
+'''
+
+'''
+Instructions
+Update the pasta_type variable to store the string "fusilli" instead of its current value.
+Update the quantity variable to store the integer 100 instead.
+'''
+
+print(pasta_type)
+print(quantity)
+
+# Update the pasta type to fusilli
+pasta_type = "fufilli"
+
+# Update the quantity to 100
+quantity = 100
+
+print(pasta_type)
+print(quantity)
